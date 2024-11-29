8b10c70d528dec39e1229075f51b91715fc6b659
dawng278 <haidang27082005@gmail.com>
2024-11-29 10:53:30 +0700

    init commit

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require('express') // này là commonjs
+const path = require('path');
 // import express from 'express'; // này là es modules
 
 const app = express() // app express
 const port = 8080 // port
 
+// config template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
 // khai báo route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  res.render('sample.ejs')
+})
+
+app.get('/abc', (req, res) => {
+  res.send('<h1>Check ABC</h1>')
 })
 
 app.listen(port, () => {
