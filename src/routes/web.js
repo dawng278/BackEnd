f4461995c562f8c237710dda76be9a1dbd6cff8b
dawng278 <haidang27082005@gmail.com>
2024-12-02 15:54:17 +0700

    init commit

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('sample.ejs')
-  })
-  
-router.get('/abc', (req, res) => {
-    res.send('<h1>Check ABC</h1>')
+    res.render('HomePage.ejs')
   })
 
   module.exports = router; //exports default
